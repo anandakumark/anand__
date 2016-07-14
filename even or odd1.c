c1a3924a15d4ccc56135c1a7fe29db9bec136348
Anand <anandakumar.cs14@bitsathy.ac.in>
2016-07-14 16:17:52 +0530

    Create even or odd1.c

---
+++ even or odd1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int p;
+ 
+   printf("Enter an integer\n");
+   scanf("%d", &n);
+ 
+   if (p%2 == 0)
+      printf("Even\n");
+   else
+      printf("Odd\n");
+ 
+   return 0;
+}
